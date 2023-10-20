a1bd489ffae9724ebb3e13f1266f49b51a809f79
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-20 13:15:03 -0500

    Javascript to read and print the content of a file.

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,20 @@
+// Import the 'fs' module to work with the file system
+const fs = require('fs');
+
+// Check if the correct number of command line arguments is provided
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-readme.js <file_path>');
+  process.exit(1); // Exit the program with an error code
+}
+
+// Get the file path from the command line argument
+const filePath = process.argv[2];
+
+// Read the content of the file in utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err); // Display the error if one occurs
+  } else {
+    console.log(data); // Display the file content if there are no errors
+  }
+});

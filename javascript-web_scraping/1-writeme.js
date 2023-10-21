06fc17793a48dfeafbda3866e9b15342e0e12f98
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-21 11:33:10 -0500

    Script that writes a string to a file.

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,20 +1,23 @@
 #!/usr/bin/node
+// Import the 'fs' module to work with the file system
 const fs = require('fs');
 
-// Check if the correct number of command-line arguments is provided
+// Check if the correct number of arguments are provided in the command line
 if (process.argv.length !== 4) {
   console.error('Usage: node 1-writeme.js <file_path> <string_to_write>');
   process.exit(1);
 }
 
+// Get the file path and the string to write from the command line arguments
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
-// Write the content to the file in UTF-8 encoding
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err); // Display the error object if an error occurs
-  } else {
-    console.log('File has been written successfully.');
-  }
-});
+// Use 'fs.promises.writeFile' to write the file in utf-8 format
+fs.promises.writeFile(filePath, stringToWrite, 'utf-8')
+  .then(() => {
+    console.log(`Data written to ${filePath}`);
+  })
+  .catch((error) => {
+    // Handle any errors that may occur during writing and print the error object
+    console.error(error);
+  });

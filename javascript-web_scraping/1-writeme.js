d1563deef7f2329f8648a52239157310f57348bf
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-21 12:11:26 -0500

    Update code

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -16,8 +16,7 @@ const stringToWrite = process.argv[3];
 try {
   // Use 'fs.writeFileSync' to write the file in utf-8 format
   fs.writeFileSync(filePath, stringToWrite, 'utf-8');
-  console.log(`Data written to ${filePath}`);
-  console.log(stringToWrite); // Add this line to display the written content
+  console.log(stringToWrite); // Display the written content directly
 } catch (error) {
   // Handle any errors that may occur during writing and print the error object
   console.error(error);

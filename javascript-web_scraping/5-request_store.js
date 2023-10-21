215e2f8a362e07d86dbe969422cfe25e01a853b0
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-21 15:14:37 -0500

    Added commits

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
+// Import the 'request' module for making HTTP requests
 const request = require('request');
+
+// Import the 'fs' module for working with the file system
 const fs = require('fs');
+
 
 // Check if the correct number of arguments (2) is provided in the command line
 if (process.argv.length !== 4) {

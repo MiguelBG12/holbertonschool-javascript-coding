405d232cc86b119fedb6e6ce8f4cc948c5999be4
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-21 15:30:25 -0500

    Updated code

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 
+// Import the 'request' module for making HTTP requests
 const request = require('request');
 
-// Define the API URL for the JSON data
-const apiUrl = process.argv[2]; // Get the API URL from the command line argument
+// Define the API URL to fetch JSON data
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
 // Make a GET request to the API URL
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(`Error: ${error}`);
-    process.exit(1);
+    process.exit(1); // Exit the script with a status code of 1 to indicate an error
   }
 
   if (response.statusCode === 200) {
+    // Parse the response body as JSON
     const todos = JSON.parse(body);
 
     // Create an object to store the count of completed tasks for each user
@@ -29,10 +31,12 @@ request(apiUrl, (error, response, body) => {
       }
     });
 
-    // Print the completed tasks object as JSON with indentation
-    console.log(JSON.stringify(completedTasksByUser, null, 2));
+    // Print the users with completed tasks
+    for (const userId in completedTasksByUser) {
+      console.log(`User ${userId} has completed ${completedTasksByUser[userId]} task(s).`);
+    }
   } else {
     console.error(`Error: Failed to retrieve task data. Status code: ${response.statusCode}`);
-    process.exit(1);
+    process.exit(1); // Exit the script with a status code of 1 to indicate an error
   }
 });

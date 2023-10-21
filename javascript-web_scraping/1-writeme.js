cc2b4a5927e17e320585105e41628f0520192cfc
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-20 20:41:34 -0500

    Update file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,27 +1,20 @@
 #!/usr/bin/node
 const fs = require('fs');
-// Import the 'fs' module to work with the file system.
 
+// Check if the correct number of command-line arguments is provided
 if (process.argv.length !== 4) {
-  // Check if the correct number of command line arguments is provided (should be 4, including the script name, file path, and string to write).
   console.error('Usage: node 1-writeme.js <file_path> <string_to_write>');
-  // If the correct number of arguments is not provided, display an error message in the console.
   process.exit(1);
-  // Exit the program with an error code.
 }
 
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
-// Get the file path and the string to write from the command line arguments.
 
+// Write the content to the file in UTF-8 encoding
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-  // Write the content to the file in UTF-8 encoding using the 'fs.writeFile' method and handle errors in a callback function.
   if (err) {
-    // If an error occurs during the writing, this block of code is executed.
-    console.error(err); // Display the error object in the console if an error occurs.
+    console.error(err); // Display the error object if an error occurs
   } else {
-    // If there are no errors, this block of code is executed.
     console.log('File has been written successfully.');
-    // Display a success message in the console.
   }
 });

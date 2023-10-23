9d5d4ac820d7ab5404321500fbabdad918f5c650
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-22 20:09:16 -0500

    Update code file

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const axios = require('axios');
 
 const movieId = process.argv[2];
 
@@ -7,35 +7,28 @@ if (!movieId) {
   process.exit(1);
 }
 
-const apiUrl = `https://swapi-api.hbtn.io/films/${movieId}/`;
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+axios
+  .get(apiUrl)
+  .then((response) => {
+    const movie = response.data;
+    console.log(`Characters in ${movie.title}:`);
+
+    const characterPromises = movie.characters.map(characterUrl =>
+      axios.get(characterUrl).then(charResponse => charResponse.data.name)
+    );
+
+    Promise.all(characterPromises)
+      .then((characterNames) => {
+        characterNames.forEach(name => console.log(name));
+      })
+      .catch((err) => {
+        console.error('Error:', err.message);
+        process.exit(1);
+      });
+  })
+  .catch((error) => {
+    console.error('Error:', error.message);
     process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Status Code: ${response.statusCode}`);
-    process.exit(1);
-  }
-
-  const movie = JSON.parse(body);
-  console.log(`Characters in ${movie.title}:`);
-
-  function getCharacter(characterUrl) {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-      } else if (charResponse.statusCode !== 200) {
-        console.error(`Status Code: ${charResponse.statusCode}`);
-      } else {
-        const character = JSON.parse(charBody);
-        console.log(character.name);
-      }
-    });
-  }
-
-  // Iterate through the characters in the movie and get their names
-  movie.characters.forEach(getCharacter);
-});
+  });

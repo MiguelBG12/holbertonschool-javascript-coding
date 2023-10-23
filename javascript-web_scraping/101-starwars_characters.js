7f226975abdecb72aaa524fd786d6fdd19f1682f
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-22 21:11:09 -0500

    Moved function declaration to function body root

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,48 +1,65 @@
 #!/usr/bin/node
 
+// Import the 'request' module for making HTTP requests
 const request = require('request');
 
+// Get the movie ID from the command line arguments
 const movieId = process.argv[2];
 
-// Check if the movie ID is provided as a command line argument
+// Check if a movie ID is provided as a command line argument
 if (!movieId) {
   console.log('You must provide the movie ID as an argument.');
   process.exit(1);
 }
 
+// Define the URL to fetch movie data based on the provided movie ID
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
+// Make an HTTP request to the specified URL
 request(apiUrl, (error, response, body) => {
-  // Check if there are no errors and the response status code is 200 (OK)
+  // Check if the request was successful (no error) and the status code is 200 (OK)
   if (!error && response.statusCode === 200) {
+    // Parse the JSON response body to get movie data
     const movieData = JSON.parse(body);
 
+    // Extract character URLs from the movie data
     const characterUrls = movieData.characters;
     let characterCount = 0;
 
-    // Define the function to print character names
+    // Define a function to print character names recursively
     function printCharacterName () {
-      // Check if there are more characters to print
+      // Check if there are more character URLs to process
       if (characterCount < characterUrls.length) {
+        // Get the URL of the next character
         const characterUrl = characterUrls[characterCount];
+
+        // Make a request to the character URL to fetch character data
         request(characterUrl, (charError, charResponse, charBody) => {
-          // Check if there are no errors and the response status code is 200
+          // Check if the character request was successful
           if (!charError && charResponse.statusCode === 200) {
+            // Parse the JSON response body to get character data
             const characterData = JSON.parse(charBody);
+
+            // Print the character's name
             console.log(characterData.name);
+
+            // Move on to the next character
             characterCount++;
-            // Call the function recursively for the next character
+
+            // Recursively call the function for the next character
             printCharacterName();
           } else {
+            // Handle errors when fetching character data
             console.error('Error getting character data:', charError);
           }
         });
       }
     }
 
-    // Start printing character names
+    // Start the process of printing character names
     printCharacterName();
   } else {
+    // Handle errors when fetching movie data
     console.error('Error getting movie data:', error);
   }
 });

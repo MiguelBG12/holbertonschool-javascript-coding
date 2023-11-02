0e0b17e653cfc92189554c1d71640c739ff0a9c0
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-02 16:51:18 -0500

    Update file, bug eslint

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,9 +1,8 @@
 // Declaration of a function named displayMessage that takes a parameter called message.
 function displayMessage(message) {
-    // We use the `console.log()` function to print the message to the standard output (STDOUT).
-    console.log(message);
-  }
-  
-  // We export the displayMessage function so it can be used in other files.
-  module.exports = displayMessage;
-  
+  // We use the `console.log()` function to print the message to the standard output (STDOUT).
+  console.log(message);
+}
+
+// We export the displayMessage function so it can be used in other files.
+module.exports = displayMessage;

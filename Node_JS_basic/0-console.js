fda11145a5ed810ea651bf93a30f5af004dc708c
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-02 16:49:04 -0500

    This code create a function named displayMessage that prints in STDOUT the string argument.

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+// Declaration of a function named displayMessage that takes a parameter called message.
+function displayMessage(message) {
+    // We use the `console.log()` function to print the message to the standard output (STDOUT).
+    console.log(message);
+  }
+  
+  // We export the displayMessage function so it can be used in other files.
+  module.exports = displayMessage;
+  

2aaf52a278884c0ee1646dcfffa983745a201765
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-05 14:23:09 -0500

    The readDatabase function reads a database asynchronously from the specified file and returns a promise. If there is an error, the promise is rejected with an error message. If successful, it resolves the promise with the grouped data.

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+// Define a function named readDatabase that reads the database asynchronously.
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  // Read the file from the specified path.
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      // If there's an error reading the file, reject the promise.
+      reject(new Error('Cannot load the database'));
+    } else {
+      // Split the data into lines and process it.
+      const lines = data.trim().split('\n');
+      const studentData = {};
+
+      // Parse each line to extract student information and group them by field.
+      lines.slice(1).forEach((line) => {
+        const [firstname, lastname, , field] = line.split(',');
+        if (field) {
+          if (!studentData[field]) {
+            studentData[field] = [];
+          }
+          studentData[field].push(firstname);
+        }
+      });
+
+      // Resolve the promise with the grouped student data.
+      resolve(studentData);
+    }
+  });
+});
+
+module.exports = readDatabase;

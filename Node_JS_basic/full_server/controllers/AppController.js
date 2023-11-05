afe7d88f7e4c9f8eac3c19566ab5ed375a8b9f03
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-05 14:30:18 -0500

    This code ceate a class named AppController. Add a static method named getHomepage. The method accepts request and response as argument. It returns a status and the message.

---
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,10 @@
+class AppController {
+    // Define a static method named getHomepage that accepts request and response.
+    static getHomepage = (req, res) => {
+      // Return a 200 status and the message "Hello Holberton School!" as the response.
+      res.status(200).send('Hello Holberton School!');
+    }
+  }
+  
+  export default AppController;
+  

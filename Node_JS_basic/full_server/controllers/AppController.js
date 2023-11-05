197bed910155ca14bdb8f92ecd3141e17c048b37
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-05 17:39:14 -0500

    Fix eslint bug

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,10 +1,9 @@
 class AppController {
-    // Define a static method named getHomepage that accepts request and response.
-    static getHomepage(req, res) {
-      // Return a 200 status and the message "Hello Holberton School!" as the response.
-      res.status(200).send('Hello Holberton School!');
-    }
+  // Define a static method named getHomepage that accepts request and response.
+  static getHomepage(req, res) {
+    // Return a 200 status and the message "Hello Holberton School!" as the response.
+    res.status(200).send('Hello Holberton School!');
+  }
 }
-  
+
 export default AppController;
-  

0ce6309382182076373759f10ccf4152a8aa7c96
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-05 21:47:16 -0500

    This code defines the StudentsController class with methods for handling student data, including reading the database, processing information, and validating parameters.

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,61 +1,88 @@
-// Import the `readDatabase` function from the '../utils' module.
+// Import the 'readDatabase' function from the 'utils' module.
 import readDatabase from '../utils';
 
-// Define the StudentsController class, which contains methods for handling students.
+// Define an array of valid major values.
+const VALID_MAJORS = ['CS', 'SWE'];
+
+// Define the 'StudentsController' class containing methods for handling students.
 class StudentsController {
   // Static method to get all students.
-  static getAllStudents(req, res) {
-    // Read the database using the third command line argument (process.argv[2).
-    readDatabase(process.argv[2])
-      .then((data) => {
-        // Process the data to get the list of students by field of study.
-        const students = data
-          .reduce((acc, curr) => {
-            const firstname = curr[0];
-            const field = curr[3];
-            if (!acc[field]) {
-              acc[field] = [];
-            }
-            acc[field].push(firstname);
-            return acc;
-          }, {});
+  static getAllStudents(request, response) {
+    // Determine the path to the data file based on the command-line arguments.
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
 
-        // Send a response with the list of students by field of study.
-        res.status(200).send(`This is the list of our students\n${
-          Object.keys(students).map((key) => `Number of students in ${key}: ${students[key].length}. List: ${students[key].join(', ')}`).join('\n')}`);
+    // Read the database using the 'readDatabase' function.
+    readDatabase(dataPath)
+      .then((studentGroups) => {
+        // Initialize an array to store response parts.
+        const responseParts = ['This is the list of our students'];
+
+        // Define a comparison function to sort groups by field name.
+        const cmpFxn = (a, b) => {
+          if (a[0].toLowerCase() < b[0].toLowerCase()) {
+            return -1;
+          }
+          if (a[0].toLowerCase() > b[0].toLowerCase()) {
+            return 1;
+          }
+          return 0;
+        };
+
+        // Iterate through student groups and construct the response.
+        for (const [field, group] of Object.entries(studentGroups).sort(cmpFxn)) {
+          responseParts.push([
+            `Number of students in ${field}: ${group.length}.`,
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
+        }
+
+        // Send a 200 status response with the joined response parts.
+        response.status(200).send(responseParts.join('\n'));
       })
-      .catch(() => {
-        // If there's an error loading the database, send an error response.
-        res.status(500).send('Cannot load the database');
+      .catch((err) => {
+        // Handle any errors and send an appropriate response.
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
       });
   }
 
   // Static method to get students by major.
-  static getAllStudentsByMajor(req, res) {
-    const { major } = req.params;
+  static getAllStudentsByMajor(request, response) {
+    // Determine the path to the data file based on the command-line arguments.
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    const { major } = request.params;
 
-    // Check if the specified major is valid.
-    if (major !== 'CS' && major !== 'SWE') {
-      res.status(500).send('Major parameter must be CS or SWE');
-    } else {
-      // Read the database using the third command line argument (process.argv[2).
-      readDatabase(process.argv[2])
-        .then((data) => {
-          // Filter students with the specified major.
-          const students = data
-            .filter((student) => student[3] === major)
-            .map((student) => student[0]);
-
-          // Send a response with the list of students with the specified major.
-          res.status(200).send(`List: ${students.join(', ')}`);
-        })
-        .catch(() => {
-          // If there's an error loading the database, send an error response.
-          res.status(500).send('Cannot load the database');
-        });
+    // Check if the provided major is valid.
+    if (!VALID_MAJORS.includes(major)) {
+      response.status(500).send('Major parameter must be CS or SWE');
+      return;
     }
+
+    // Read the database using the 'readDatabase' function.
+    readDatabase(dataPath)
+      .then((studentGroups) => {
+        let responseText = '';
+
+        // Check if the major is present in the student groups.
+        if (Object.keys(studentGroups).includes(major)) {
+          const group = studentGroups[major];
+          responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
+        }
+
+        // Send a 200 status response with the response text.
+        response.status(200).send(responseText);
+      })
+      .catch((err) => {
+        // Handle any errors and send an appropriate response.
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
+      });
   }
 }
 
-// Export the StudentsController class so it can be used in other modules.
+// Export the 'StudentsController' class for use in other modules.
 export default StudentsController;
+module.exports = StudentsController;

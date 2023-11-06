3d4f2d9a9783c9be61f5365cd2bba36605c7787f
MiguelBG12 <miguel.bg12@gmail.com>
2023-11-05 21:37:43 -0500

    Update code file

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,15 +1,20 @@
 import express from 'express';
-import routes from './routes';
+import mapRoutes from './routes';
 
+// Create an instance of the Express application.
 const app = express();
+
+// Define the port where the server will listen for incoming requests.
 const PORT = 1245;
 
-// Use the defined routes for the Express app.
-app.use('/', routes);
+// Call the 'mapRoutes' function to configure the application's routes.
+mapRoutes(app);
 
-// Start the server and listen on the specified port.
+// Start the server and make it listen on the specified port.
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
 
+// Export the Express application so it can be used in other modules.
 export default app;
+module.exports = app;

6f50aaef47a826974f6471985b032310e26218c2
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-17 17:38:49 -0500

    Fux  bug in commits

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-
-// Import the 'fs' module
 const fs = require('fs');
-
-// Read the content of a file passed as an argument
-fs.readFile(process.argv[2], 'utf8', function(error, content) {
-  // Log the error if present, otherwise log the content of the file
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
 });
